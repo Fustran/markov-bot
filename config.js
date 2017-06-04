4a9c900c4315f4de15c8ed71735bbe64c1974a1a
Fustran <fustra1@gmail.com>
2017-06-04 16:47:07 -0400

    command support, db support, etc.

--- config.js
+++ config.js
@@ -1,6 +1,24 @@
-var config = {
+const fs = require('fs');
+
+var settings;
+if (fs.existsSync('settings.json')) {
+    settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+} else {
+    let defaultSettings = {'nickname': 'Markov-bot', 'speak': true};
+    settings = JSON.stringify(defaultSettings, null, 4);
+    fs.writeFile('settings.json', settings, (err) => {
+        if (err) return console.warn(err);
+    });
+}
+
+let config = {
     token: process.env.MARKOV_BOT_TOKEN,
-    commandChar: process.env.MARKOV_COMMAND_CHAR
+    name:  settings.nickname || 'Markov-bot',
+    commandChar: process.env.MARKOV_COMMAND_CHAR || '?',
+    pgName: process.env.MARKOV_PGNAME,
+    pgUser: process.env.MARKOV_PGUSER,
+    pgPass: process.env.MARKOV_PGPASS,
+    pgPort: process.env.MARKOV_PGPORT
 };
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const discord = require('discord.js');
 const config = require('./config.js');
 const bot = new discord.Client();
-
 require('./lib/listeners.js')(bot);
 
 bot.login(config.token);
